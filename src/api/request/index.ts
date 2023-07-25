943683524d085cfb02a0a4de2b4c5c4cca298ea7
ALLEWAN3 <allen.al.wang@dbschenker.com>
2023-07-25 10:06:29 +0800

    feat: redirect

--- src/api/request/index.ts
+++ src/api/request/index.ts
@@ -1,73 +1,69 @@
-import axios, { AxiosRequestHeaders } from 'axios';
-import storage from '@/utils/storage';
-import { ElMessage } from 'element-plus';
+import axios, { AxiosRequestHeaders } from 'axios'
+import storage from '@/utils/storage'
+import { ElMessage } from 'element-plus'
+import router from '@/router'
 
 const toastErrorMsg = (msg: string) => {
-//   toast({
-//     title: msg,
-//     status: 'error',
-//     position: 'top'
-//   })
-ElMessage.error({message: msg})
+  ElMessage.error({ message: msg })
 }
 export const request = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
-  timeout: 60000,
-});
+  timeout: 60000
+})
 
 request.interceptors.request.use(
   (config) => {
     if (typeof window !== 'undefined') {
-      const token = storage.getItem('token');
+      const token = storage.getItem('token')
       console.log('token:', token)
       if (token) {
-        config.headers!.Authorization = `Bearer ${token}`;
+        config.headers!.Authorization = `Bearer ${token}`
       }
     }
-    return config;
+    return config
   },
 
   () => {
-    throw new Error('发起请求出错');
+    throw new Error('发起请求出错')
   }
-);
+)
 
 request.interceptors.response.use(
   (data) => {
     const res = data.data
     if (!/2\d{2}/.test(res.code)) {
-        toastErrorMsg(res.message);
+      toastErrorMsg(res.message)
       return res.message
     }
 
-    return res.data;
+    return res.data
   },
   (err) => {
     console.log('err data:', err)
     if (err && err.response && err.response.status) {
-      const status = err.response.status;
+      const status = err.response.status
 
       switch (status) {
         case 504:
         case 404:
-          typeof window !== 'undefined' && toastErrorMsg('服务器异常');
-          break;
+          typeof window !== 'undefined' && toastErrorMsg('服务器异常')
+          break
         case 400:
           toastErrorMsg(err.response.data.message)
-          break;
+          break
         case 401:
           console.log('sssh')
           toastErrorMsg(err.response.data.message)
-          break;
+          router.push('/login')
+          break
 
         default:
-          typeof window !== 'undefined' &&
             toastErrorMsg(
               (err.response && err.response.data && err.response.data.msg) || '未知错误!'
-            );
+            )
       }
     }
 
-    return Promise.reject(err);
+    return Promise.reject(err)
   }
 )
